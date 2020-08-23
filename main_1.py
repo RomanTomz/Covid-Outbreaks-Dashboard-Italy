4a7d9138083ba6a71c18a73da98c929f60a52b3d
Tommaso Di Marcello <t.dimarcello@gmail.com>
2020-08-23 07:09:00 +0100

    Modifiche Data Prep

--- main_1.py
+++ main_1.py
@@ -27,13 +27,12 @@ def load_data_p():
 
 data_p = load_data_p()
 data_p = data_p[~(data_p.denominazione_provincia == 'In fase di definizione/aggiornamento') & ~(data_p.denominazione_provincia == 'Fuori Regione / Provincia Autonoma')]
-data_p['pct_crescita'] = data_p.groupby('denominazione_provincia')['totale_casi'].transform(lambda x :(x.pct_change()*100).round(2))
-data_p['media_mobile'] = data_p.groupby('denominazione_provincia')['pct_crescita'].apply(lambda x : x.rolling(window=7).mean().round(2))
+data_p['pct_crescita'] = data_p.groupby('denominazione_provincia')['totale_casi'].transform(lambda x :(x.pct_change(periods=7)*100).round(2))
 data_p['nuovi_casi_7gg'] = data_p.groupby('denominazione_provincia')['totale_casi'].apply(lambda x:x.diff(periods=7))
 
 prov_latest = data_p.set_index('data').sort_index().groupby('denominazione_provincia').tail(1)
 top5_nc7 = prov_latest.sort_values('nuovi_casi_7gg', ascending=False).head(5)
-top5_mm = prov_latest.sort_values('media_mobile', ascending=False).head(5)
+top5_mm = prov_latest.sort_values('pct_crescita', ascending=False).head(5)
 # Data cleaning and preparation (regioni)
 @st.cache(persist=True, allow_output_mutation=True)
 def load_data_r():
@@ -89,20 +88,21 @@ for feature in regioni_geo['features']:
   feature['id'] = feature['properties']['NOME_REG']
 
 #Data Prep per tabelle
-top5_mm.rename(columns={'denominazione_provincia':'Provincia', 'media_mobile':'Crescita % Media Mobile 7gg'}, inplace=True)
+top5_mm.rename(columns={'denominazione_provincia':'Provincia', 'pct_crescita':'Crescita %  7gg'}, inplace=True)
 top5_nc7.rename(columns={'denominazione_provincia':'Provincia', 'nuovi_casi_7gg':'Nuovi Casi in 7gg'}, inplace=True)
 top5_mm.set_index('Provincia', inplace=True)
 top5_nc7.set_index('Provincia', inplace=True)
 
 
-formatted_mm = top5_mm[['Crescita % Media Mobile 7gg']].style.background_gradient(cmap='Reds', ).format('{0:,.2f}')
+formatted_mm = top5_mm[['Crescita %  7gg']].style.background_gradient(cmap='Reds', ).format('{0:,.2f}')
 formatter_nc7 = top5_nc7[['Nuovi Casi in 7gg']].style.background_gradient(cmap='Reds', ).format('{0:,.0f}')
 
 
 # Mappa nuovi casi
+prov_max = (prov_latest.groupby('denominazione_provincia')['nuovi_casi_7gg'].max()).max()
 fig_geo_7 = go.Figure(go.Choroplethmapbox(geojson=province_geo, locations=data_p.denominazione_provincia, z=data_p.nuovi_casi_7gg,
-                                    colorscale="Reds", zmin=0, zmax=400,
-                                    marker_opacity=0.5, marker_line_width=0.6))
+                                    colorscale="Reds", zmin=0, zmax=prov_max-100,
+                                    marker_opacity=1, marker_line_width=0.6))
 fig_geo_7.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=4, mapbox_center = {"lat": 41.8719, "lon": 12.5674})
 fig_geo_7.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
@@ -110,10 +110,12 @@ fig_geo_7.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
 
 
 #Mappa media Mobile
+mm_max = (prov_latest.groupby('denominazione_provincia')['pct_crescita'].max()).max()
+mm_min = (prov_latest.groupby('denominazione_provincia')['pct_crescita'].min()).min()
 
-fig_geo_mm = go.Figure(go.Choroplethmapbox(geojson=province_geo, locations=data_p.denominazione_provincia, z=data_p.media_mobile,
-                                    colorscale="Reds", zmin=0, zmax=1.5,
-                                    marker_opacity=0.5, marker_line_width=0.6))
+fig_geo_mm = go.Figure(go.Choroplethmapbox(geojson=province_geo, locations=data_p.denominazione_provincia, z=data_p.pct_crescita,
+                                    colorscale="Reds", zmin=mm_min, zmax=mm_max,
+                                    marker_opacity=1, marker_line_width=0.6))
 fig_geo_mm.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=4, mapbox_center = {"lat": 41.8719, "lon": 12.5674})
 fig_geo_mm.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
@@ -122,7 +124,7 @@ fig_geo_mm.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
 extremum_2 = max(np.max(reg_latest.crescita_ospedalizzati_sett), np.abs(np.min(reg_latest.crescita_ospedalizzati_sett)))
 fig_osp = go.Figure(go.Choroplethmapbox(geojson=regioni_geo, locations=reg_latest.denominazione_regione, z=reg_latest.crescita_ospedalizzati_sett,
                                     colorscale="temps", zmin=-extremum_2,zmid=0, zmax=extremum_2,
-                                    marker_opacity=0.5, marker_line_width=0.6))
+                                    marker_opacity=0.7, marker_line_width=0.6))
 fig_osp.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=4, mapbox_center = {"lat": 41.8719, "lon": 12.5674})
 fig_osp.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
@@ -131,7 +133,7 @@ fig_osp.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
 extremum_3 = max(np.max(reg_latest.indice_pos), np.abs(np.min(reg_latest.indice_pos)))
 fig_ind = go.Figure(go.Choroplethmapbox(geojson=regioni_geo, locations=reg_latest.denominazione_regione, z=reg_latest.indice_pos,
                                     colorscale="Temps", zmin=-extremum_3,zmid=0, zmax=extremum_3,
-                                    marker_opacity=0.5, marker_line_width=0.6))
+                                    marker_opacity=0.7, marker_line_width=0.6))
 fig_ind.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=4, mapbox_center = {"lat": 41.8719, "lon": 12.5674})
 fig_ind.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
@@ -139,7 +141,7 @@ fig_ind.update_layout(margin={"r":0,"t":20,"l":0,"b":0})
 
 st.sidebar.header('Tipo Visualizzazione')
 
-viz = st.sidebar.radio('Seleziona Visualizzazione',('Nuovi Casi 7 gg','Media Mobile 7 gg','Incremento Ospedalizzati 7 gg (Regionale)','Indice Positivitá 7gg (Regionale)'))
+viz = st.sidebar.radio('Seleziona Visualizzazione',('Nuovi Casi 7 gg','Crescita %  in 7 gg','Incremento Ospedalizzati 7 gg (Regionale)','Indice Positivitá 7gg (Regionale)'))
 
 
 if viz == 'Nuovi Casi 7 gg':
@@ -148,31 +150,31 @@ if viz == 'Nuovi Casi 7 gg':
         st.markdown('#### Nuovi Casi Negli Ultimi 7 Giorni - Visualizzazione Geografica')
         st.plotly_chart(fig_geo_7)
 
-if viz == 'Media Mobile 7 gg':
-        st.markdown('Prime 5 Province per Maggiore Incremento Media Mobile a 7 Giorni')
+if viz == 'Crescita %  in 7 gg':
+        st.markdown('Prime 5 Province per Maggiore Incremento Percentuale Contagi a 7 Giorni')
         st.write(formatted_mm)
-        st.markdown('#### Crescita Media Mobile Negli Ultimi 7 Giorni - Visualizzazione Geografica')
+        st.markdown('#### Crescita Percentuale Contagi Negli Ultimi 7 Giorni - Visualizzazione Geografica')
         st.plotly_chart(fig_geo_mm)
 
 if viz == 'Incremento Ospedalizzati 7 gg (Regionale)':
-        st.markdown('#### Crescita Ospedalizzati Negli Ultimi 7 Giorni')
+        st.markdown('#### Prime 5 Regioni per Crescita Ospedalizzati Negli Ultimi 7 Giorni')
         st.write(top5_osp)
         st.markdown('#### Crescita Ospedalizzati Negli Ultimi 7 Giorni - Visualizzazione Geografica')
         st.plotly_chart(fig_osp)
 
 if viz == 'Indice Positivitá 7gg (Regionale)':
-        st.markdown('#### Indice di Positivitá Negli Ultimi 7 Giorni')
+        st.markdown('#### Prime 5 Regioni per Indice di Positivitá Negli Ultimi 7 Giorni')
         st.write(indice)
         st.markdown('#### Indice di Positivitá a 7 Giorni - Visualizzazione Geografica')
         st.plotly_chart(fig_ind)
 
 
 st.sidebar.markdown(
-    ("#### La Dashboard utilizza la media mobile e l'incremento a 7 giorni per dare maggiore chiarezza al trend eliminando le oscillazioni quotidiane")
+    ("#### La Dashboard utilizza l'incremento a 7 giorni per dare maggiore chiarezza al trend eliminando le oscillazioni quotidiane")
 
 )
 
-st.sidebar.markdown("#### L'indice di positivitá é ottenuto con: **(nuovi_positivi_7gg/casi_testati_7gg)x100 **")
+st.sidebar.markdown("#### L'indice di positivitá é ottenuto con:  **(nuovi_positivi_7gg/casi_testati_7gg)x100 ** ")
 
 st.sidebar.markdown("###### Dati da https://github.com/pcm-dpc/COVID-19 ")
 st.sidebar.button('Aggiorna Dati')
